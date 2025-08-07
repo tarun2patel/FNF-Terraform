98bd2021391c766a44f74465dc163be5a5d3791f
tarun2patel <72042732+tarun2patel@users.noreply.github.com>
2025-08-07 19:33:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,19 +22,44 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
- 
-  name                     = "techtutorial101"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location # implicit dependency
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# Create Network Security Group
+resource "azurerm_network_security_group" "example" {
+  name                = var.environment == "dev" ? "dev-nsg" : "stage-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-  tags = {
-    environment = local.common_tags.environment
+  # Here's where we need the dynamic block
+  dynamic "security_rule" {
+    for_each = local.nsg_rules
+    content {
+      name                       = security_rule.key
+      priority                   = security_rule.value.priority
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range         = "*"
+      destination_port_range    = security_rule.value.destination_port_range
+      source_address_prefix     = "*"
+      destination_address_prefix = "*"
+      description               = security_rule.value.description
+    }
   }
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.example.name
+# Output the security rules
+output "security_rules" {
+  value = azurerm_network_security_group.example.security_rule
+}  
+
+output "env" {
+  value = var.environment
+}
+
+
+output "demo" {
+  value = [ for count in local.nsg_rules : count.description ]
+}
+
+output "splat" {
+  value = var.account_names[1]
 }
